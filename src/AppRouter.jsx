1c66ebd6ed291bc80dd8adfec3316a39b08ace37
Caio de Souza Ramos <109087473+Caioramos010@users.noreply.github.com>
2025-08-18 21:41:27 -0300

    fix: vite environment

---
+++ src/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Curriculo from './pages/Curriculo';
+import Artigos from './pages/Artigos';
+
+function AppRouter() {
+  // Define o basename baseado no ambiente
+  const basename = process.env.NODE_ENV === 'production' ? '/Portifolio' : '';
+  
+  return (
+    <Router basename={basename}>
+      <Routes>
+        <Route path="/Portifolio" element={<Home />} />
+        <Route path="/curriculo" element={<Curriculo />} />
+        <Route path="/artigos" element={<Artigos />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default AppRouter;
